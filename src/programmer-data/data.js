19a1ed66c3a1a4c617dd96b58538ab55a50b40f1
rimonspi12 <rimonmpi@gmail.com>
2021-09-25 23:54:06 +0600

    project file added

---
+++ src/programmer-data/data.js
@@ -0,0 +1,142 @@
+const programmer = [
+    {
+        Rankig: '1',
+        name: 'Alan Mathison Turing',
+        Role: 'scientist, mathematician, cryptanalyst, and logician',
+        country: 'British',
+        salary: '5500'
+    },
+    {
+        Rankig: '2',
+        name: 'Dennis Ritchie',
+        Role: 'Computer scientist.',
+        country: 'United States',
+        salary: '6000'
+    },
+    {
+        Rankig: '3',
+        name: 'Linus Torvalds',
+        Role: 'software engineer.',
+        country: 'United States',
+        salary: '7000'
+    },
+    {
+        Rankig: '4',
+        name: 'Bjarne Stroustrup',
+        Role: 'computer scientist',
+        country: 'Aarhus Denmark',
+        salary: '7500'
+    },
+    {
+        Rankig: '5',
+        name: 'Bjarne Stroustrup',
+        Role: 'computer scientist',
+        country: 'London United Kingdom',
+        salary: '8000'
+    },
+    {
+        Rankig: '6',
+        name: 'Ken Thompson',
+        Role: 'American computer designer',
+        country: 'United States',
+        salary: '7200'
+    },
+    {
+        Rankig: '7',
+        name: 'Brian Kernighan',
+        Role: 'Canadian computer scientist',
+        country: 'United States',
+        salary: '8300'
+    },
+    {
+        Rankig: '8',
+        name: 'Bill Gates',
+        Role: 'American Entrepreneur,software developer,',
+        country: 'United States',
+        salary: '9000'
+    },
+    {
+        Rankig: '9',
+        name: 'John Backus',
+        Role: 'American computer scientist',
+        country: 'United States',
+        salary: '8360'
+    },
+    {
+        Rankig: '10',
+        name: 'Grace Hopper',
+        Role: 'American computer scientist',
+        country: 'United States',
+        salary: '8460'
+    },
+    {
+        Rankig: '11',
+        name: 'John G. Kemeny',
+        Role: 'Hungarian-American mathematician',
+        country: 'Hungary',
+        salary: '8260'
+    },
+    {
+        Rankig: '12',
+        name: 'Thomas E. Kurtz',
+        Role: 'Professor of mathematics',
+        country: 'United States',
+        salary: '8530'
+    },
+    {
+        Rankig: '13',
+        name: 'John McCarthy',
+        Role: 'American computer scientist',
+        country: 'United States',
+        salary: '7330'
+    },
+    {
+        Rankig: '14',
+        name: ' Alan Kay',
+        Role: 'American computer scientist',
+        country: 'United States',
+        salary: '5330'
+    },
+    {
+        Rankig: '15',
+        name: 'Niklaus Wirth',
+        Role: 'Swiss computer scientist',
+        country: 'Switzerland',
+        salary: '6250'
+    },
+    {
+        Rankig: '16',
+        name: 'James Gosling',
+        Role: 'Canadian computer scientist',
+        country: 'Canada',
+        salary: '7250'
+    },
+    {
+        Rankig: '17',
+        name: 'Guido van Rossum',
+        Role: 'Dutch programmer',
+        country: 'Haarlem Netherlands',
+        salary: '7450'
+    },
+    {
+        Rankig: '18',
+        name: 'Donald Knuth',
+        Role: 'American computer scientist',
+        country: 'United States',
+        salary: '6250'
+    },
+    {
+        Rankig: '19',
+        name: ' John Carmack',
+        Role: 'CTO of Oculus VR',
+        country: 'United States',
+        salary: '5250'
+    },
+    {
+        Rankig: '20',
+        name: 'David Patterson',
+        Role: 'Distinguished Engineer',
+        country: 'Illions,United States',
+        salary: '6250'
+    },
+]
